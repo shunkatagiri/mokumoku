6f37291a46374f4a3bf6a21e35a42688b59d6ddd
shunkatagiri <shun.katagiri1214@gmail.com>
2023-03-28 19:03:34 +0900

    - ユーザーが女性以外の場合、イベント作成時にOnly womanは表示されず、女性限定イベントが作成できない。

--- app/controllers/events/attendances_controller.rb
+++ app/controllers/events/attendances_controller.rb
@@ -3,16 +3,20 @@
 class Events::AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
+    if @event.only_woman && current_user.gender != '女性'
+      redirect_back(fallback_location: root_path, alert: 'このイベントは女性限定です')
+    else
     event_attendance = current_user.attend(@event)
     (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
       NotificationFacade.attended_to_event(event_attendance, user)
     end
     redirect_back(fallback_location: root_path, success: '参加の申込をしました')
+    end
   end
 
   def destroy
     @event = Event.find(params[:event_id])
     current_user.cancel_attend(@event)
     redirect_back(fallback_location: root_path, success: '申込をキャンセルしました')
-  end
+  end 
 end
